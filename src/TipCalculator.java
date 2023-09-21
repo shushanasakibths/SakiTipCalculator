3e29d8434ca6b0e79cc4a83bf92fcc25c19f774b
BT_1N3_30 <sample@bths.com>
2023-09-21 10:07:34 -0400

    initial commit, welcome and first variables initialized

---
+++ src/TipCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("This is the tip calculator!");
+        System.out.println("---------------------------");
+        System.out.print("Enter the number of people in your group: ");
+        int numberOfPeople = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter the tip percentage as an integer: ");
+        int tipPercentage = scan.nextInt();
+        scan.nextLine();
+        System.out.print("Please enter the price of an item you've ordered:  ");
+        int tipPercentage = scan.nextInt();
+        scan.nextLine();
+
+
+    }
+}
